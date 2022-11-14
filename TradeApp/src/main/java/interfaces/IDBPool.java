961c0d6304677cdf81a2734ae8bda4dfb46e20ac
NoFuture <56985998+Steamsurver@users.noreply.github.com>
2022-11-14 16:10:01 +0700

    Delete TradeApp directory

--- TradeApp/src/main/java/interfaces/IDBPool.java
+++
@@ -1,9 +0,0 @@
-package interfaces;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface IDBPool {
-	Connection getConnection() throws SQLException;
-	void returnConnection(Connection conn) throws SQLException;
-}
