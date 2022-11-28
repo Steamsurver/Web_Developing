0d10a848acadeef0041ce7a6e15e8e860b9c21bd
NoFuture <56985998+Steamsurver@users.noreply.github.com>
2022-11-28 20:20:31 +0700

    Delete TradeApp directory

--- TradeApp/WebApplication2/src/main/java/Game_product.java
+++
@@ -1,8 +0,0 @@
-
-public class Game_product {
-	public int id;
-	public int cost;
-	public String game_name;
-	public String desc;
-	public String img_src;
-}
--- TradeApp/src/main/java/classes/Game_product.java
+++
@@ -1,9 +0,0 @@
-package classes;
-
-public class Game_product {
-	public int id;
-	public int cost;
-	public String game_name;
-	public String desc;
-	public String img_src;
-}
